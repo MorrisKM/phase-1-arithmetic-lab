afb6e5be6c66f9f9edf0b39b4322e5fbb7c40905
MorrisKM <itsmorrismunene@gmail.com>
2025-06-10 12:27:27 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 124;
+let num2 = 0.5;
+const multiply = (num1 * num2);
+const random = Math.floor(Math.random()*10);
+let num3 = 10;
+let num4 = 6;
+const mod = (num3 % num4);
+let max = Math.max(10,15,20,12);
